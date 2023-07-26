2c5635d83a3a9eefcecec832aeb3816b9dd7813b
Licsis <gaston.caceres@educacionadventista.org.ar>
2023-07-26 15:32:55 -0300

    Primer subida de archivos a GIT

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Prueba</title>
+</head>
+<body>
+  <Div>
+    <h1>Hola Mundo</h1>
+    <a href="Proyecto_Equipos/principal.php">Proyecto de los equipos</a>
+  </Div>
+</body>
+</html>
